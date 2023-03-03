af6bcc2c44576f4324f2cd0f6e2edb99275e5a96
Sergey Ilin <ilin_sergey90@mail.ru>
2023-03-03 23:53:58 +0300

    add BD Flask-SQLAlchemy

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 import requests
 from bs4 import BeautifulSoup
+
+from webapp.model import db, News
 
 
 def get_html(url: str):
@@ -16,15 +20,21 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, "html.parser")
         all_news = soup.find("ul", class_="list-recent-posts").findAll("li")
-        result_news = []
         for news in all_news:
             title = news.find("a").text
             url = news.find("a")["href"]
             published = news.find("time").text
-            result_news.append({
-                "title": title,
-                "url": url,
-                "published": published
-            })
-        return result_news
-    return False
+            print(published)
+            try:
+                published = datetime.strptime(published, "%b. %d, %Y") 
+                print(published)               
+            except ValueError:
+                published = datetime.now()                 
+            save_news(title, url, published)
+
+def save_news(title, url, published):
+    news_exists = News.query.filter(News.url == url).count()
+    if not news_exists:
+        news_news = News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
